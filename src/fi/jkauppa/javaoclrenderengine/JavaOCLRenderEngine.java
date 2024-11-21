5a0a6b64dbb3c05f2abc45c1456a80daac9d354e
goofyseeker311 <goofyseeker311@gmail.com>
2024-11-21 05:45:28 +0200

    Updated ship.obj and shiptexture.png/.jpg
    Updated res/models/ship.obj to use 4096x4096 res/models/shiptexture.png test texture.

--- src/fi/jkauppa/javaoclrenderengine/JavaOCLRenderEngine.java
+++ src/fi/jkauppa/javaoclrenderengine/JavaOCLRenderEngine.java
@@ -34,7 +34,7 @@ import fi.jkauppa.javarenderengine.ModelLib.Triangle;
 import fi.jkauppa.javarenderengine.UtilLib;
 
 public class JavaOCLRenderEngine {
-	private static String programtitle = "Java OpenCL Render Engine v1.0.6.4";
+	private static String programtitle = "Java OpenCL Render Engine v1.0.6.5";
 	private int screenwidth = 0, screenheight = 0, graphicswidth = 0, graphicsheight = 0, graphicslength = 0;
 	private float graphicshfov = 70.0f, graphicsvfov = 39.375f;
 	private long window = MemoryUtil.NULL;
