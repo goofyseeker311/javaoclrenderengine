fa5d27c1878aa8768116c5da3fed99691c4ba83b
goofyseeker311 <goofyseeker311@gmail.com>
2024-12-04 19:50:38 +0200

    Testing commit
    Any possible changes that were not committed and version number change.

--- src/fi/jkauppa/javaoclrenderengine/JavaOCLRenderEngine.java
+++ src/fi/jkauppa/javaoclrenderengine/JavaOCLRenderEngine.java
@@ -43,7 +43,7 @@ import fi.jkauppa.javarenderengine.ModelLib.Triangle;
 import fi.jkauppa.javarenderengine.UtilLib;
 
 public class JavaOCLRenderEngine {
-	private static String programtitle = "Java OpenCL Render Engine v1.1.0.8";
+	private static String programtitle = "Java OpenCL Render Engine v1.1.0.9";
 	private int screenwidth = 0, screenheight = 0, graphicswidth = 0, graphicsheight = 0, graphicslength = 0;
 	@SuppressWarnings("unused")
 	private int litgraphicswidth = 0, litgraphicsheight = 0;
