0db2cb8dbbdd8da3493f590534695404d995f44e
goofyseeker311 <goofyseeker311@gmail.com>
2025-02-22 06:37:15 +0200

    Updated JavaOCLRenderEngine
    Changed to filtered 1/4 native resolution rendering for speed and upscaling test (450-550fps at 720p and 200fps at 4K).

--- src/fi/jkauppa/javaoclrenderengine/JavaOCLRenderEngine.java
+++ src/fi/jkauppa/javaoclrenderengine/JavaOCLRenderEngine.java
@@ -46,7 +46,7 @@ import fi.jkauppa.javarenderengine.UtilLib;
 
 public class JavaOCLRenderEngine {
 	private Random rand = new Random();
-	private static String programtitle = "Java OpenCL Render Engine v1.2.2.0";
+	private static String programtitle = "Java OpenCL Render Engine v1.2.2.1";
 	private int screenwidth = 0, screenheight = 0, graphicswidth = 0, graphicsheight = 0, graphicslength = 0;
 	@SuppressWarnings("unused")
 	private int litgraphicswidth = 0, litgraphicsheight = 0;
@@ -131,8 +131,8 @@ public class JavaOCLRenderEngine {
 			this.screenwidth = videomode.width();
 			this.screenheight = videomode.height();
 		}
-		this.graphicswidth = screenwidth*1;
-		this.graphicsheight = screenheight*1;
+		this.graphicswidth = screenwidth/2;
+		this.graphicsheight = screenheight/2;
 		if (vglinterop==0) {
 			this.glinterop = false;
 		}
