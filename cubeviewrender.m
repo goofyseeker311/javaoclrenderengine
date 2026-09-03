b8bfa82beff6232caa7bcdb5e715cd01401c7c46
goofyseeker311 <goofyseeker311@gmail.com>
2026-09-03 20:10:26 +0300

    Update cubeviewrender.m
    added planepointdistace() function. other functions are normalizevector() and planefromnormalatpoint().

--- cubeviewrender.m
+++ cubeviewrender.m
@@ -8,9 +8,16 @@ function plane = planefromnormalatpoint(normal,point)
   plane = [normal -dot(normal,point)];
 endfunction
 
+function distance = planepointdistance(plane,point)
+  distance = dot(plane,[point 1]);
+endfunction
+
 v = [4 2 1];
 vn = normalizevector(v);
 p = [-1 2 7];
 pl = planefromnormalatpoint(vn,p);
 pt = pl * [p 1]';
+s = [-3 2 7 5];
+d = planepointdistance(pl,s(1:3));
+h = abs(d)<s(4);
 
